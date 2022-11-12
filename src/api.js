fc5f9976a6c83c6850e00eaa73ec47e06bbbc7e1
damianglejzer <damianglejzer92@gmail.com>
2022-11-12 11:39:57 +0100

    deployed

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export 
+const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8ca47d62bemsh3db8a532d5bd651p16102fjsn4645be065816',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e0d23a7f1a76168a36764486d1eeac56"
